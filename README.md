5a77c958448002df68fbd1518b438e3bdf819cb7
Alexandre Crépu <crepu.alexandre@hotmail.fr>
2023-01-29 17:40:00 +0100

    (FEAT) Fulll FizzBuzz algo test

---
+++ README.md
@@ -0,0 +1,16 @@
+# fizz-buzz-fulll
+
+Afficher la page html algo_fulll.html afin de voir l'exécution du FizzBuzz
+
+Possibilité de le voir dans une application VueJS en exécutant les commandes suivantes dans le dossier **fizz-buzz-fulll**
+Se rendre sur localhost:8080 (le port peut changer s'il est occupé sur votre poste, voir le  résultat dans la console)
+
+## Installer les dépendances
+```
+yarn install
+```
+
+### Exécuter l'application
+```
+yarn serve
+```
---
+++ fizz-buzz-fulll/src/components/FizzBuzz.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row>
+    <v-list v-for="key in fizzBuzzData" :key=key cols="12">
+      <v-col>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>{{key.count}} => {{ key.item }}</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-col>
+    </v-list>
+  </v-row>
+</template>
+
+<script>
+
+  export default {
+    name: 'FizzBuzz',
+    mounted() {
+      this.startFizzBuzz()
+    },
+
+    methods: {
+      startFizzBuzz() {
+        // On commence à 1, 0 modulo un chiffre/nombre renverra toujours 0
+        for (let i = 1; i < this.fizzBuzzCounter; i++) {
+          let data = {};
+          if (i % 15 == 0) {
+              data.item = "FizzBuzz";
+          } else if (i % 3 == 0) {
+              data.item = "Fizz";
+          } else if (i % 5 == 0) {
+              data.item = "Buzz";
+          } else {
+              data.item = i;
+          }
+          data.count = i;
+          this.fizzBuzzData.push(data);
+        }
+      }
+    },
+
+    data: () => ({
+      fizzBuzzCounter: 100,
+      fizzBuzzData: []
+    })
+  };
+
+</script>
